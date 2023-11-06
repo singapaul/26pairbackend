960461ac920a2432e5a250d0473513b6347f9fd8
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2023-11-06 09:03:58 +0000

    Deploy

---
+++ src/app/api/dailyShuffle.js/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+ 
+export function GET(request) {
+
+    // body: request.body,
+
+console.log('set up a cron job on vercel')
+
+
+  return NextResponse.json(
+    {
+      body: "Daily shuffle saved to database",
+      path: request.nextUrl.pathname,
+      query: request.nextUrl.search,
+      cookies: request.cookies.getAll(),
+    },
+    {
+      status: 200,
+    },
+  );
+}
